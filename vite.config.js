663e556e0a9d48fdac748b2c66db2b2dcf4ad8a9
muteeb-tahir <139177794+muteeb-tahir@users.noreply.github.com>
2025-06-08 17:02:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react' // if you're using React
 
 export default defineConfig({
-  base: '/again/',  
+  base: '/new/',  
   plugins: [react()],
 })
